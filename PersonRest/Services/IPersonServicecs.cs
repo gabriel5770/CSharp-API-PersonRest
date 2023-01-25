d06984106e0ef224cf12769adefe2c770ae502ff
Gabriel dos Santos Vieira <gabriel.vieira@3stecnologia.com.br>
2023-01-25 09:46:14 -0300

    Adicionar arquivos de projeto.

---
+++ PersonRest/Services/IPersonServicecs.cs
@@ -0,0 +1,15 @@
+﻿
+using PersonRest.Model;
+
+namespace PersonRest.Services
+{
+    public interface IPersonServicecs
+    {
+        Person Create(Person person); 
+        Person FindBydID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+
+    }
+}
