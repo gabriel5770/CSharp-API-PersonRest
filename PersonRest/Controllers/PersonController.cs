b6fea271bd801f16b5945a8ffc3f0f02ecb01d10
Gabriel dos Santos Vieira <gabriel.vieira@3stecnologia.com.br>
2023-01-26 11:24:36 -0300

    PersonController.cs: Implementação dos verbos; Startup.cs: Inserção do esqueleto da classe;

--- PersonRest/Controllers/PersonController.cs
+++ PersonRest/Controllers/PersonController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using PersonRest.Model;
+using PersonRest.Services;
 
 namespace PersonRest.Controllers
 {
@@ -7,10 +9,48 @@ namespace PersonRest.Controllers
     public class PersonController : ControllerBase
     {
         private readonly ILogger<PersonController> _logger;
+        private IPersonService _personService;
 
-        public PersonController(ILogger<PersonController> logger)
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
         {
             _logger = logger;
+            _personService = personService;
+        }
+
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            return Ok(_personService.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindBydID(id);
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personService.Create(person));
+        }
+
+        [HttpPost]
+        public IActionResult put([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personService.Update(person));
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+             return NoContent();
         }
     }
 }
---
+++ PersonRest/Startup.cs
@@ -0,0 +1,27 @@
+﻿using PersonRest.Services;
+using PersonRest.Services.Implementations;
+
+namespace PersonRest
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            
+            //Adiciona a Interface e a implementação
+            //Injeção de dependência
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
+        }
+
+
+
+
+    }
+}
