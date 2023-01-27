2892fa03d65b2b0e5a9bceb2ca67da73bdde86e7
gabriel santos Vieira <GabrielsantosV2@hotmail.com>
2023-01-26 22:12:08 -0300

    Configuração da classe Startup.cs

--- PersonRest/Startup.cs
+++ PersonRest/Startup.cs
@@ -14,10 +14,33 @@ namespace PersonRest
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            
+
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
             //Adiciona a Interface e a implementação
             //Injeção de dependência
             services.AddScoped<IPersonService, PersonServiceImplementation>();
+
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment enviroment)
+        {
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
         }
 
 
